6c6bea84f2e924238a890d7a320f4d1ca221f73c
comingforyou21 <haianh6101991@gmail.com>
2021-05-11 10:09:46 +0700

    1

--- i/1.php
+++ i/1.php
@@ -1,13 +1,27 @@
+<style>
+	input[type="submit"] , input[type="number"] , input[type="text"]{ display: block; padding: 10px; margin: 10px; }
+	input[type="submit"] {  }
+</style>
+<form action="" method="POST">
+	<input type="text" placeholder="Path" name="path">
+	<input type="number" placeholder="STT" name="stt">
+	<input type="submit" value="Submit" name="submit">
+</form>
+
 <?php
 
-// your folder name, here I am using templates in root
-$directory = '';
-$i=0;
-foreach (glob($directory."*.jpg") as $filename) {
-	$i++;
-    $file = realpath($filename);
-    //var_dump($file);
-    rename($file, $i.".jpg");
-}
+if (!empty($_POST["submit"])) {
+
+	$path = $_POST["path"];
+	$i=$_POST["stt"];
+	foreach (glob($path."/*.jpg") as $filename) {
+		
+		$file = realpath($filename);    
+		rename($file, $path . "/". $i.".jpg");
+		$i++;
+	}
+} 
+
+
 
 ?>
